3b8a2a84af04b3ae3b7f41a563a746950715b7ae
676632 <676632@stud.hvl.no>
2024-01-25 10:21:14 +0100

    ok

--- f05-eksempler-foertimen/src/dat102/f05/app/baklengs/BaklengsMain.java
+++ f05-eksempler-foertimen/src/dat102/f05/app/baklengs/BaklengsMain.java
@@ -1,12 +1,25 @@
 package dat102.f05.app.baklengs;
 
+import dat102.f05.stabel.StabelADT;
+import dat102.f05.stabel.tabellstabel.TabellStabel;
+
 public class BaklengsMain {
 	
 	public static void main(String[] args) {
 		
 		String[] tabell = {"Anne", "Per", "Lise", "Knut"};
 		
-		//TODO fra scratch
+		StabelADT<String> stabelen = new TabellStabel<>();
+		
+		for (String s : tabell) {
+			stabelen.push(s);
+			
+		}
+		
+		while (!stabelen.isEmpty()) {
+			String s = stabelen.pop();
+			System.out.println(s);
+		}
 		
 	}
 	
